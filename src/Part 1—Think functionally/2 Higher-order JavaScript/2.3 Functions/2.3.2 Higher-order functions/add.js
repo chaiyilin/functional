32cd049605f71ec7c7e13b8eb4247ac9258a4a79
Eric Chai <eric.chai@tollgroup.com>
2017-01-16 17:07:45 +1100

    init

--- src/Part 1—Think functionally/2 Higher-order JavaScript/2.3 Functions/2.3.2 Higher-order functions/add.js
+++
@@ -1,6 +0,0 @@
-function add(a) {
-    return function (b) {
-        return a + b;
-    }
-}
-add(3)(3); //-> 6
--- src/Part 1—Think functionally/2 Higher-order JavaScript/2.3 Functions/2.3.2 Higher-order functions/applyOperation.js
+++
@@ -1,5 +0,0 @@
-function applyOperation(a, b, opt) {
-    return opt(a, b);
-}
-var multiplier = (a, b) => a * b;
-applyOperation(2, 3, multiplier); // -> 6
