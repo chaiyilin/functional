25d9f7dc9bdcc960d384385389b4f0659ebdaa09
Eric Chai <eric.chai@tollgroup.com>
2017-04-19 15:40:53 +1000

    init

--- src/features/chain/v_final_3.js
+++ src/features/chain/v_final_3.js
@@ -1,9 +1,9 @@
 const {map,flatten,sortBy,flow}= require('lodash/fp');
-//import {map,flatten,sortBy,flow} from 'material-ui/Divider';
+//import {map,flatten,sortBy,flow} from 'lodash/fp';
 const resultFunc=flow(
     map(x => [x, x*2]),
     flatten,
     sortBy(x => x)
 );
-const result = flow(resultFunc, resultFunc)([1, 2, 3]);
+const result = flow(resultFunc, resultFunc,map(x => [x, x*2]))([1, 2, 3]);
 console.log(result);
